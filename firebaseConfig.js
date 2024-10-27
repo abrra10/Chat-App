a799b47312bee5f6100790b066f3199833237f09
Benacer Taha Walid <88546423+abrra10@users.noreply.github.com>
2024-10-27 19:53:10 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,12 +7,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBaDtYcdApq652IN3fiXv6iGdrWWTHBUYU",
-    authDomain: "chatapp-eba20.firebaseapp.com",
-    projectId: "chatapp-eba20",
-    storageBucket: "chatapp-eba20.appspot.com",
-    messagingSenderId: "601598600940",
-    appId: "1:601598600940:web:e1cf2f6c67529310c167c9"
+    apiKey: "",
+    authDomain: "",
+    projectId: "c",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 
